b5ed515ac91d34b9658d4a600a4a566363e61a78
Roman Prokhorov <roma.smaug@gmail.com>
2025-10-29 22:41:07 +0200

    feat: basic interface (can be changed during implementation)

---
+++ include/csv_handler.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "student.hpp"
+
+namespace csv {
+    /**
+     * @brief Read student data from CSV file
+     * @param filename Path to the CSV file
+     * @return Vector of Student objects
+     */
+    std::vector<Student> read_csv(const std::string& filename);
+    
+    /**
+     * @brief Write student data to CSV file
+     * @param filename Path to the output CSV file
+     * @param students Vector of Student objects to write
+     * @return true if successful, false otherwise
+     */
+    bool write_csv(const std::string& filename, const std::vector<Student>& students);
+    
+    /**
+     * @brief Parse a single line from CSV
+     * @param line CSV line to parse
+     * @return Student object
+     */
+    Student parse_line(const std::string& line);
+    
+    /**
+     * @brief Convert Student object to CSV line
+     * @param student Student object to convert
+     * @return CSV formatted string
+     */
+    std::string to_csv_line(const Student& student);
+}
---
+++ include/student.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+struct Student {
+    std::string m_name;         // Name
+    std::string m_surname;      // Surname
+    std::string m_email;        // [a-z.]*@student.org
+    int m_birth_year;           // 1950..2010
+    int m_birth_month;          // 1..12
+    int m_birth_day;            // 1..Number of days in m_birth_month
+    std::string m_group;        // [A-Z][A-Z][A-Z]-[0-9][0-9]
+    float m_rating;             // 0..100
+    std::string m_phone_number; // 38(0xx)xx-xx-xxx
+    
+    /**
+     * @brief Default constructor
+     */
+    Student();
+    
+    /**
+     * @brief Parameterized constructor
+     */
+    Student(const std::string& name, const std::string& surname, const std::string& email,
+            int birth_year, int birth_month, int birth_day, const std::string& group,
+            float rating, const std::string& phone_number);
+    
+    /**
+     * @brief Get full name
+     * @return Full name (surname + name)
+     */
+    std::string get_full_name() const;
+        
+};
+
+/**
+ * @brief Comparison functions for Student sorting
+ */
+namespace student_comparators {
+    /**
+     * @brief Compare students by surname, then by name (alphabetically)
+     * Used for Task 2: sorting students by surname and name
+     */
+    bool compare_by_surname_and_name(const Student& a, const Student& b);
+    
+    /**
+     * @brief Compare students by rating (ascending order)
+     * Used for Task 4: sorting all students by rating
+     */
+    bool compare_by_rating(const Student& a, const Student& b);
+    
+    /**
+     * @brief Compare students by rating (descending order)
+     */
+    bool compare_by_rating_desc(const Student& a, const Student& b);
+    
+    // Additional comparators
+    bool compare_by_name(const Student& a, const Student& b);
+    bool compare_by_surname(const Student& a, const Student& b);
+    bool compare_by_email(const Student& a, const Student& b);
+    bool compare_by_birth_date(const Student& a, const Student& b);
+    bool compare_by_group(const Student& a, const Student& b);
+    bool compare_by_rating(const Student& a, const Student& b);
+    bool compare_by_rating_desc(const Student& a, const Student& b);
+}
+
+/**
+ * @brief Overload stream operator for output
+ */
+std::ostream& operator<<(std::ostream& os, const Student& student);
